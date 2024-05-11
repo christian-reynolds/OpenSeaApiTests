d3a82301d2243ce0db179690e6d5ceb5f6c36bd6
Christian Reynolds <christian.reynolds@cryptoslam.io>
2024-05-11 06:27:43 -0400

    Added filtering to get sales from the BM Finale collection only for Julian Hespenheide

--- sales.js
+++ sales.js
@@ -14,10 +14,11 @@ const unixTimestampInSeconds = Math.floor(unixTimestamp / 1000);
 console.log("Current datetime as Unix epoch timestamp (milliseconds):", unixTimestamp);
 console.log("Current datetime as Unix epoch timestamp (seconds):", unixTimestampInSeconds);
 
-// The param after is hardcoded to 9/1/2022 12AM GMT.  This is the day the collection was created
+// The param after is hardcoded to 9/1/2022 12AM GMT. This is the day the collection was created
+const creationDateUnix = 1661990400; // Unix timestamp for September 1, 2022
 const eventType = 'sale';
-const baseParams = `after=1661990400&before=${unixTimestampInSeconds}&event_type=${eventType}&limit=50`;
-const collectionSlug = 'kernels-by-julian-hespenheide';
+const baseParams = `after=${creationDateUnix}&before=${unixTimestampInSeconds}&event_type=${eventType}&limit=50`;
+const collectionSlug = 'bright-moments-finale';
 const fileName = `${collectionSlug}-${eventType}.json`;
 const baseUrl = `https://api.opensea.io/api/v2/events/collection/${collectionSlug}?${baseParams}`;
 
@@ -32,6 +33,9 @@ const options = {
 
 let allEvents = []; // Array to store all fetched events
 
+// Specify the desired NFT identifiers
+const targetIdentifiers = new Set(["147", "199", "257", "276", "345", "479", "545", "582", "642", "682", "687", "835", "896", "90", "913", "100"]);
+
 async function fetchEvents(nextCursor) {
     let url = baseUrl;
     if (nextCursor) {
@@ -40,7 +44,18 @@ async function fetchEvents(nextCursor) {
 
     try {
         const response = await axios.get(url, options);
-        allEvents.push(...response.data.asset_events); // Store the asset events into the array
+        
+        const retrievedEvents = response.data.asset_events;
+        
+        // Proceed with filtering if matching the specific slug
+        if (collectionSlug === 'bright-moments-finale') {
+            const filteredEvents = retrievedEvents.filter(event => {
+                return event.nft && targetIdentifiers.has(event.nft.identifier);
+            });
+            allEvents.push(...filteredEvents); // Store the filtered events into the array
+        } else {
+            allEvents.push(...retrievedEvents);
+        }
 
         if (response.data.next) {
             console.log('Fetching next page...');
